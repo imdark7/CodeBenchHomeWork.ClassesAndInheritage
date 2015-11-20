3b8d320f9c0bd1632302c105eaa43beb2e56bf8e
imdark7 <imdark@yandex.ru>
2015-11-20 17:25:14 +0500

    FirstCommit

---
+++ src/Dog.java
@@ -0,0 +1,27 @@
+
+public class Dog {
+    private int age;
+    private String passCode;
+    private String breed;
+    public Dog(int age, String breed) {
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public String getPassCode() {
+        return passCode;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public void setPassCode(String passCode) {
+        this.passCode = passCode;
+    }
+    public void bark() {
+        System.out.println("гав-гав-гав");
+    }
+
+}
